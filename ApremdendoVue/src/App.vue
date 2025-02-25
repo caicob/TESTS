71e505657f994ac34bc472318b2f1af4afb72e7d
caicob <30500958+caicob@users.noreply.github.com>
2025-02-25 08:11:23 +0000

    vue testes

---
+++ ApremdendoVue/src/App.vue
@@ -0,0 +1,51 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    
+    <div class="wrapper">
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+
+  <var-row :gutter="[10, 10]">
+    <var-col :span="8">
+      <div class="item">span: 8</div>
+    </var-col>
+    <var-col :span="8">
+      <div class="item">span: 8</div>
+    </var-col>
+    <var-col :span="8">
+      <div class="item">span: 8</div>
+    </var-col>
+    <var-col :span="8">
+      <div class="item">span: 8</div>
+    </var-col>
+    <var-col :span="8">
+      <div class="item">span: 8</div>
+    </var-col>
+    <var-col :span="8">
+      <div class="item">span: 8</div>
+    </var-col>
+  </var-row>
+</template>
+
+<style>
+.item {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 36px;
+  background: #3a7afe;
+  color: #fff;
+}
+</style>
---
+++ ApremdendoVue/src/main.js
@@ -0,0 +1,14 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import Varlet from '@varlet/ui'
+import App from './App.vue'
+import router from './router'
+import '@varlet/ui/es/style'
+
+const app = createApp(App)
+
+app.use(router)
+app.use(Varlet)
+
+app.mount('#app')
